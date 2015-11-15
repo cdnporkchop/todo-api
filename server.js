e4f5276493f6197ca8d24a5913e6a1921ed484c2
cdnporkchop <cdn.porkchop@gmail.com>
2015-11-14 22:43:20 -0500

    Added PUT method to update todos

--- server.js
+++ server.js
@@ -56,15 +56,46 @@ app.post('/todos', function (req, res) {
 
 app.delete('/todos:id', function (req, res) {
 	var todoId = parseInt(req.params.id);
-	var matchedTodo = _.findWhere(todos, {id: todoId});	
+	var matchedTodo = _.findWhere(todos, {'id': todoId});	
 
 	if (!matchedTodo) {
 		res.status(404).json({"error":"no todo found with that id"});
 	} else {
 		todos = _.without(todos, matchedTodo);
 		res.json(matchedTodo);	
+	}	
+});
+
+app.put('/todos:id', function (req, res) {
+	var todoId = parseInt(req.parms.id);
+	var matchedTodo = _.findWhere(todos, {'id': todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if (!matchedTodo) {
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		// bad, provided but not boolean
+		return res.status(400).send();
+	} else {
+		// Never provided attribute, no problem here	
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		// bad, provided but string or len > 0
+		return res.status(400).send();
+	} else {
+		// Never provided attribute, no problem here	
 	}
 	
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
 });
 
 app.listen(PORT, function () {
