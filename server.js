caa8cf7961a21b6b18cf40677eacf78f8861776d
cdnporkchop <cdn.porkchop@gmail.com>
2015-11-14 22:11:24 -0500

    Refactoring with underscore.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -18,14 +19,13 @@ app.get('/todos', function (req, res) {
 
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
-	todos.forEach(function (todo) {
-		if (todo.id === todoId) {
-			matchedTodo = todo;
-		}
-	});
-
+	// todos.forEach(function (todo) {
+	// 	if (todo.id === todoId) {
+	// 		matchedTodo = todo;
+	// 	}
+	// });
 	if (matchedTodo) {
 		res.json(matchedTodo);
 	} else {
@@ -34,7 +34,16 @@ app.get('/todos/:id', function (req, res) {
 });
 
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	// var body = req.body;
+
+	// body = _.pick(body, 'description', 'status', "id");
+	var body = _.pick(req.body, 'description', 'status', "id");
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
 
 	body.id = todoNextId++; //= todoNextId; // or = todoNextId++;
 	//todoNextId++;
