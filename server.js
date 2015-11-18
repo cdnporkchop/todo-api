68937ad64720c547b19a83761b09fd54cc7e8ead
cdnporkchop <cdn.porkchop@gmail.com>
2015-11-17 19:45:25 -0500

    db.todos to db.todo

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/todos', function(req, res) {
 		}
 	} 
 
-	db.todos.findAll({where: where}).then(function (todos) {
+	db.todo.findAll({'where': where}).then(function (todos) {
 		res.json(todos);
 	}, function (e) {
 		res.status(500).send(e);
